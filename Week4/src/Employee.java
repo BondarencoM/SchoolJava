e3733c87aa30d2f68e6471b60efda7d49cf4faa5
bondarencom <bondarenco.mihail@gmail.com>
2019-02-11 17:45:54 +0200

    fix(week4) add files for solved Ex2

---
+++ Week4/src/Employee.java
@@ -0,0 +1,24 @@
+public class Employee implements Taxes {
+	private double baseSalary = 40000;
+	private int years;
+	public Employee(int years)
+	{ 
+		this.years = years;
+	}
+	public double getBonus() 
+	{
+		return years*1000;
+	}
+	@Override
+	public double getGrossSalary() {
+	
+		return baseSalary + getBonus()  ;
+	}
+	
+	@Override
+	public double getNetSalary() {
+		double gs = getGrossSalary();
+		return gs - gs * Taxes.getTaxRate(gs);
+		
+	}
+}
